c4f3648f18258e1579b16a41958cf6acd4d81c51
nicolas billard <nicolasbillard.dev@gmail.com>
2026-09-22 16:53:58 +0200

    ajout commentaire à la classe Discount

--- app/services/discount.py
+++ app/services/discount.py
@@ -1,16 +1,24 @@
 from app.models.movie import Movie
 
 class Discount:
+    """
+    Application d'une réduction sur les films.
+    15€ pour un film Back to the Future (BTTF), 
+    10% de réduction pour 2 films BTTF distincts, 
+    20% de réduction pour 3 films BTTF distincts.
+    """
     BTTF_MOVIES = [f'back to the future {i}' for i in ['1','2','3']]
     BTTF_PRICE = 15
     def __init__(self):
         self.movies = []
 
     def count_distinct_bttf_movies(self):
+        """Compte le nombre de films BTTF distincts dans la liste de films fournie."""
         distinct_movies = set([movie.title for movie in self.movies])
         return len(distinct_movies & set(self.BTTF_MOVIES))
 
     def set_discount_price(self, count_bttf_movies: int):
+        """Détermine le prix réduit en fonction du nombre de films BTTF distincts."""
         match count_bttf_movies:
             case 2:
                 return self.BTTF_PRICE * (1 - 0.1)
@@ -21,6 +29,7 @@ class Discount:
 
 
     def apply(self, movies: list[Movie]):
+        """Applique le prix réduit sur les films BTTF"""
         self.movies = movies
         n_bttf_movies = self.count_distinct_bttf_movies()
         for movie in self.movies:
